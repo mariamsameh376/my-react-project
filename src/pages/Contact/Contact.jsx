0f8c4d4427fb133d9d93547cb5c5981ac7fec4e8
mariamsameh376 <romasameh580@gmail.com>
2025-08-10 06:32:12 -0700

    initiall commit

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'; // المسار حسب مكان Navbar فعليًا
 import Footer from '../../components/Footer/Footer'; // نفس الشيء
-import ContactUs from'../../components/ContactUS/ContactUS';
+import ContactUs from'../../components/ContactUs/ContactUs';
 
 const Contact = () => {
     return (
